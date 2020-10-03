4e2ba56182cd4f4fe525780381dd584567ab13c3
Womsxd <45663319+Womsxd@users.noreply.github.com>
2020-10-03 13:42:48 +0800

    Update ys_UserInfoGet.py

--- ys_UserInfoGet.py
+++ ys_UserInfoGet.py
@@ -10,7 +10,7 @@ def GetInfo(Uid):
             'Accept-Language': 'zh-CN,zh;q=0.9,zh-HK;q=0.8',
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36'
         }
-        )
+    )
     return req.text
 
 def JsonAnalysis(JsonText):
